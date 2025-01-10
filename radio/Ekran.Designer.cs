52ac8a82c8efb8a443b9c394f236484d710a625e
enboz <enesb33@outlook.com>
2025-01-10 20:02:52 +0300

    çalışır bir kod oluşturuldu yorum satırı eklendi birkaç ince ayar yapıldı

---
+++ radio/Ekran.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace radio
+{
+    partial class Ekran
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            s_s = new Button();
+            url = new TextBox();
+            SuspendLayout();
+            // 
+            // s_s
+            // 
+            s_s.Location = new Point(12, 12);
+            s_s.Name = "s_s";
+            s_s.Size = new Size(94, 29);
+            s_s.TabIndex = 0;
+            s_s.Text = "button1";
+            s_s.UseVisualStyleBackColor = true;
+            s_s.Click += s_s_Click;
+            // 
+            // url
+            // 
+            url.Location = new Point(112, 12);
+            url.Name = "url";
+            url.Size = new Size(257, 27);
+            url.TabIndex = 1;
+            // 
+            // Ekran
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 62);
+            Controls.Add(url);
+            Controls.Add(s_s);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Ekran";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ekran";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button s_s;
+        private TextBox url;
+    }
+}
---
+++ radio/Ekran.cs
@@ -0,0 +1,56 @@
+using System.Net.Http.Headers;
+using NAudio.Wave;
+
+namespace radio
+{
+    public partial class Ekran : Form
+    {
+        //deyiþkenler ayarlanýr biri çalma iþlemi diyeri veriyi okumak için kullanýlýr
+        private WaveOutEvent wçýktý;
+        private MediaFoundationReader mokuyucu;
+        //eski url yi tutar
+        private String esurl = "a";
+        //önceden basýlýp basýlmadýðýna bakar
+        private Boolean basýldý = false;
+        public Ekran()
+        {
+            InitializeComponent();
+        }
+
+        //butona basma durumunda olacaklar
+        private void s_s_Click(object sender, EventArgs e)
+        {
+            
+            String urll = url.Text;
+            try
+            {
+                if (!basýldý)
+                {
+                    if (esurl.Equals(urll))
+                    {
+                        wçýktý.Play();
+                    }
+                    else
+                    {
+                        mokuyucu = new MediaFoundationReader(urll);
+                        wçýktý = new WaveOutEvent();
+                        wçýktý.Init(mokuyucu);
+                        wçýktý.Play();
+                    }
+                    basýldý = true;
+                }
+                else
+                {
+                    //durdurma iþlemi
+                    wçýktý.Stop();
+                    basýldý = false;
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"bir hata var :\n {ex.Message}","hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
